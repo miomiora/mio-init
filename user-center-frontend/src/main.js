56a8d9b7b85601b018b0596285d47a69c3bff18e
miomiora <1729089090@qq.com>
2023-02-28 00:09:17 +0800

    试试上传前端

--- user-center-frontend/src/main.js
+++ user-center-frontend/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import routes from './router/routes'
+import 'element-plus/theme-chalk/index.css'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(App).use(router).mount('#app')
---
+++ user-center-frontend/src/pages/Login.vue
@@ -0,0 +1,72 @@
+<template>
+    <h1>登录页面</h1>
+    <div style="width: 300px">
+      <el-input v-model="user_account" placeholder="请输入用户名"  />
+      <p></p>
+      <el-input v-model="user_password" placeholder="请输入密码" show-password/>
+      <p></p>
+      <el-button type="primary" @click="login" >登录</el-button>
+      <p></p>
+      <el-button type="warning" @click="toRegister" >-> 注册页面</el-button>
+    </div>
+
+
+</template>
+
+<script setup>
+import {onMounted, ref} from 'vue'
+import request from "../plugin/request";
+import {useRouter} from "vue-router";
+import { ElMessage } from 'element-plus'
+
+const router = useRouter()
+let user_account = ref('')
+let user_password = ref('')
+
+
+async function login() {
+  request.post('login', {
+    user_account: user_account.value,
+    user_password: user_password.value,
+  }).then(res => {
+    if( res.code === 0 ) {
+      ElMessage.success('登录成功')
+      // 设置Token
+      if (res.data.user.role === 1) {
+        router.push('adminPage')
+      } else if (res.data.user.role === 0) {
+        router.push('userPage')
+      }
+      localStorage.setItem('token', res.data.token);
+    } else {
+      ElMessage.error(res.message)
+    }
+  });
+}
+
+function toRegister() {
+  router.push('register')
+}
+
+async function getCurrentUser() {
+  request.get('current').then(res => {
+    if( res.code === 0 ) {
+      ElMessage.success('用户已经登录')
+      if(res.data.role === 0) {
+        router.push('userPage')
+      } else if (res.data.role === 1) {
+        router.push('adminPage')
+      }
+    }
+  });
+}
+
+onMounted( () => {
+  getCurrentUser();
+})
+
+</script>
+
+<style scoped>
+
+</style>
